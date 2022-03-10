d35a6101d13c7835c41f4c9f2e0c6eabe1cebb68
torikushiii <joerdyl@gmail.com>
2022-03-10 19:01:09 +0800

    now properly detects if tweet is longer than usual

--- src/stream.js
+++ src/stream.js
@@ -3,8 +3,8 @@ const Twitter = require("twit");
 
 module.exports = class Stream {
     /** @type {Twitter} */
-    #active = false;
     #server = null;
+    #active = false;
     #stream = null;
     #data = null;
 
@@ -98,9 +98,14 @@ module.exports = class Stream {
 
     buildEmbed (tweet, guild) {
         let embed;
+        let extended = false;
         let msg = tweet.text;
         if (tweet.is_quote_status && tweet?.extended_tweet?.full_text) {
             msg = `Quote @${tweet.quoted_status.user.screen_name}: ${tweet.extended_tweet.full_text}`;
+        }
+        else if (tweet?.extended_tweet?.full_text) {
+            msg = tweet.extended_tweet.full_text;
+            extended = true;
         }
         else if (tweet.retweeted_status?.extended_tweet?.full_text) {
             msg = `RT @${tweet.retweeted_status.user.screen_name} ${tweet.retweeted_status.extended_tweet.full_text}`;
@@ -122,40 +127,79 @@ module.exports = class Stream {
             }
         ];
 
-        if (tweet?.entities?.media?.length && tweet?.extended_entities?.media.length === 1) {
-            embed[0].image = {
-                url: tweet.entities.media[0].media_url_https
+        if (extended) {
+            if (tweet?.extended_tweet?.entities?.media?.length && tweet?.extended_tweet?.extended_entities?.media?.length === 1) {
+                embed[0].image = {
+                    url: tweet.extended_tweet.entities.media[0].media_url_https
+                }
+            }
+            else if (tweet?.extended_tweet?.extended_entities?.media?.length > 1) {
+                const urls = [];
+                for (const media of tweet.extended_entities.media) {
+                    urls.push({
+                        url: "https://twitter.com",
+                        image: {
+                            url: media.media_url_https
+                        }
+                    });
+
+                }
+
+                embed = [
+                    {
+                        color: 0x0099FF,
+                        author: {
+                            name: `${tweet.user.name} (@${tweet.user.screen_name})`,
+                            icon_url: tweet.user.profile_image_url_https
+                        },
+                        url: "https://twitter.com",
+                        description: msg,
+                        timestamp: new Date(tweet.created_at),
+                        footer: {
+                            text: "Twitter",
+                            icon_url: "https://i.imgur.com/pl2r3ng.png"
+                        }
+                    },
+                    ...urls
+                ]
             }
         }
-        else if (tweet?.extended_entities?.media && tweet?.extended_entities?.media?.length > 1) {
-            const urls = [];
-            for (const media of tweet.extended_entities.media) {
-                urls.push({
-                    url: "https://twitter.com",
-                    image: {
-                        url: media.media_url_https
-                    }
-                });
-
+        else {
+            if (tweet?.entities?.media?.length && tweet?.extended_entities?.media.length === 1) {
+                embed[0].image = {
+                    url: tweet.entities.media[0].media_url_https
+                }
             }
-
-            embed = [
-                {
-                    color: 0x0099FF,
-                    author: {
-                        name: `${tweet.user.name} (@${tweet.user.screen_name})`,
-                        icon_url: tweet.user.profile_image_url_https
+            else if (tweet?.extended_entities?.media && tweet?.extended_entities?.media?.length > 1) {
+                const urls = [];
+                for (const media of tweet.extended_entities.media) {
+                    urls.push({
+                        url: "https://twitter.com",
+                        image: {
+                            url: media.media_url_https
+                        }
+                    });
+    
+                }
+    
+                embed = [
+                    {
+                        color: 0x0099FF,
+                        author: {
+                            name: `${tweet.user.name} (@${tweet.user.screen_name})`,
+                            icon_url: tweet.user.profile_image_url_https
+                        },
+                        url: "https://twitter.com",
+                        description: msg,
+                        timestamp: new Date(tweet.created_at),
+                        footer: {
+                            text: "Twitter",
+                            icon_url: "https://i.imgur.com/pl2r3ng.png"
+                        }
                     },
-                    url: "https://twitter.com",
-                    description: msg,
-                    timestamp: new Date(tweet.created_at),
-                    footer: {
-                        text: "Twitter",
-                        icon_url: "https://i.imgur.com/pl2r3ng.png"
-                    }
-                },
-                ...urls
-            ]
+                    ...urls
+                ]
+            }
         }
 
         const text = `New tweet from ${tweet.user.screen_name}: ${this.#type[guild.type]}/${tweet.user.screen_name}/status/${tweet.id_str}`;
