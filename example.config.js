88d428adf57486c10463dedbf2e405808aa3a2d8
torikushiii <joerdyl@gmail.com>
2022-03-11 23:53:35 +0800

    added `MONGO_IP` and `MONGO_PORT`

--- example.config.js
+++ example.config.js
@@ -5,6 +5,9 @@
     process.env.ACCESS_TOKEN_SECRET = "";
     process.env.BEARER = ""; // Required to use for getting user information when adding a user to the list
 
+    process.env.MONGO_IP = "0.0.0.0";
+    process.env.MONGO_PORT = "27017"; // Default MongoDB port
+
     process.env.DISCORD_TOKEN = "";
 
     process.env.PREFIX = "$";
