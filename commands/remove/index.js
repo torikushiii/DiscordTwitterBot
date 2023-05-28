38134d8f1f587546faa6b7403998701e2dcb4d40
torikushiii <torikushiii@gmail.com>
2023-05-28 21:40:15 +0800

    Initial Implementation

--- commands/remove/index.js
+++ commands/remove/index.js
@@ -1,6 +1,52 @@
 module.exports = {
 	name: "remove",
 	aliases: [],
-	description: "Remove user from watchlist",
-	code: (async function remove () {})
+	params: [],
+	description: "Remove a user from the timeline fetcher.",
+	code: (async function remove (context, ...args) {
+		if (args.length === 0) {
+			return {
+				success: false,
+				reply: "No user specified."
+			};
+		}
+
+		const guildData = await app.Cache.getByPrefix(`discord-guilds-${context.channel.guild.id}`);
+		const { channels } = guildData;
+
+		const removed = [];
+		const skipped = [];
+
+		for (const user of args) {
+			const exists = channels.find(i => i.username === user);
+			if (exists) {
+				removed.push(user);
+				channels.splice(channels.indexOf(exists), 1);
+			}
+			else {
+				skipped.push(user);
+			}
+		}
+
+		await app.Cache.setByPrefix(`discord-guilds-${context.channel.guild.id}`, guildData, { expiry: 0 });
+
+		if (removed.length === 0 && skipped.length === 0) {
+			return {
+				success: false,
+				reply: "No user(s) were removed."
+			};
+		}
+
+		if (skipped.length > 0 && removed.length === 0) {
+			return {
+				success: false,
+				reply: `No user(s) were removed. ${skipped.length} user(s) were skipped: ${skipped.join(", ")}.`
+			};
+		}
+
+		return {
+			success: true,
+			reply: `Removed ${removed.length} user(s) from the timeline fetcher.`
+		};
+	})
 };
