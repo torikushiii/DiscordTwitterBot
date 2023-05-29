b994405fdf9bf179067719357aaa4adc29bff34f
torikushiii <torikushiii@gmail.com>
2023-05-30 04:12:42 +0800

    Initial Implementation

---
+++ commands/list/index.js
@@ -0,0 +1,25 @@
+module.exports = {
+	name: "list",
+	aliases: [],
+	params: [],
+	description: "List all currently active subscriptions for this server.",
+	code: (async function list (context) {
+		const guildData = await app.Cache.getByPrefix(`discord-guilds-${context.channel.guild.id}`);
+		const { channels } = guildData;
+
+		if (channels.length === 0) {
+			return {
+				success: false,
+				reply: "No users are currently subscribed."
+			};
+		}
+
+		const userList = channels.map(i => i.username).join(", ");
+
+		return {
+			success: true,
+			reply: `Currently subscribed to ${channels.length} user(s): ${userList}`
+		};
+	}),
+	usage: []
+};
