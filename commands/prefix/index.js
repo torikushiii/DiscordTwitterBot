9bba4be3d17a16a70c0eea7b8e483de9e3c39682
torikushiii <torikushiii@gmail.com>
2023-06-02 03:11:09 +0800

    linted

--- commands/prefix/index.js
+++ commands/prefix/index.js
@@ -1,33 +1,33 @@
 module.exports = {
 	name: "prefix",
 	aliases: [],
-    params: [],
+	params: [],
 	description: "Change the prefix for the bot.",
 	code: (async function prefix (context, target) {
 		const guildData = await app.Cache.getByPrefix(`discord-guilds-${context.channel.guild.id}`);
-        const { prefix } = guildData;
+		const { prefix } = guildData;
 
-        if (!target) {
-            return {
-                success: true,
-                reply: `The current prefix is \`${prefix}\`.`
-            };
-        }
+		if (!target) {
+			return {
+				success: true,
+				reply: `The current prefix is \`${prefix}\`.`
+			};
+		}
 
-        if (target.length > 2) {
-            return {
-                success: false,
-                reply: "The prefix must be 2 characters or less."
-            };
-        }
+		if (target.length > 2) {
+			return {
+				success: false,
+				reply: "The prefix must be 2 characters or less."
+			};
+		}
 
-        guildData.prefix = target;
-        await app.Cache.setByPrefix(`discord-guilds-${context.channel.guild.id}`, guildData, { expiry: 0 });
+		guildData.prefix = target;
+		await app.Cache.setByPrefix(`discord-guilds-${context.channel.guild.id}`, guildData, { expiry: 0 });
 
-        return {
-            success: true,
-            reply: `The prefix has been changed to \`${target}\`.`
-        };
+		return {
+			success: true,
+			reply: `The prefix has been changed to \`${target}\`.`
+		};
 	}),
 	usage: null
 };
