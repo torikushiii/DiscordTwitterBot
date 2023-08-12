c7106d1ef88666e654d685ab3fbd6400d8c6f815
torikushiii <torikushiii@gmail.com>
2023-08-12 23:19:16 +0800

    simplified `events` code

--- core/singletons/sentinel/events.js
+++ core/singletons/sentinel/events.js
@@ -1,8 +1,7 @@
 const parser = require("./tweet-processor");
 const { EventEmitter } = require("events");
 
-// eslint-disable-next-line no-useless-constructor, max-statements-per-line
-class EventManager extends EventEmitter { constructor () { super(); } }
+class EventManager extends EventEmitter {}
 const events = new EventManager();
 module.exports = events;
 
@@ -15,64 +14,51 @@ events.on("new-tweet", async (tweetData) => {
 	const userId = tweetData.user.id_str;
 	for (const guild of guilds) {
 		const guildData = await app.Cache.getByPrefix(guild);
-
 		const { channels } = guildData;
 		if (channels.length === 0) {
 			continue;
 		}
 
 		for (const channel of channels) {
-			if (channel.userId === userId) {
-				const userData = await app.User.get(channel.username);
-				if (!userData) {
-					app.Logger.warn(`User ${channel.username} not found (?)`);
-					continue;
-				}
-
-				const parsedTweet = await parser(tweetData);
-				if (Date.parse(parsedTweet.createdAt) < Date.now() - 300000) {
-					continue;
-				}
-
-				const text = `New tweet from ${userData.username}: https://twitter.com/${userData.username}/status/${parsedTweet.id}`;
-				const embeds = [
-					{
-						color: 0x0099FF,
-						author: {
-							name: `${userData.name} (@${userData.username})`,
-							icon_url: userData.avatar
-						},
-						url: "https://twitter.com/",
-						description: parsedTweet.text,
-						timestamp: new Date(parsedTweet.createdAt),
-						footer: {
-							text: "Twitter",
-							icon_url: "https://i.imgur.com/pl2r3ng.png"
-						}
-					}
-				];
-
-				const medias = [];
-				if (parsedTweet?.media?.length > 0) {
-					for (const media of parsedTweet.media) {
-						medias.push({
-							url: "https://twitter.com/",
-							image: {
-								url: media.url
-							}
-						});
-					}
-				}
-
-				if (medias.length === 1) {
-					embeds[0].image = medias[0].image;
-				}
-				else if (medias.length > 1) {
-					embeds.push(...medias);
-				}
-
-				await app.Discord.send(text, { id: channel.channelId }, { embeds });
+			if (channel.userId !== userId) {
+				continue;
 			}
+
+			const userData = await app.User.get(channel.username);
+			if (!userData) {
+				app.Logger.warn(`User ${channel.username} not found (?)`);
+				continue;
+			}
+
+			const parsedTweet = await parser(tweetData);
+			if (Date.parse(parsedTweet.createdAt) < Date.now() - 300000) {
+				continue;
+			}
+
+			const { username, name, avatar } = userData;
+			const text = `New tweet from ${username}: https://twitter.com/${username}/status/${parsedTweet.id}`;
+			const embeds = [
+				{
+					color: 0x0099FF,
+					author: {
+						name: `${name} (@${username})`,
+						icon_url: avatar
+					},
+					url: "https://twitter.com/",
+					description: parsedTweet.text,
+					timestamp: new Date(parsedTweet.createdAt),
+					footer: {
+						text: "Twitter",
+						icon_url: "https://i.imgur.com/pl2r3ng.png"
+					}
+				},
+				...(parsedTweet?.media?.map((media) => ({
+					url: "https://twitter.com/",
+					image: { url: media.url }
+				})) || [])
+			];
+
+			await app.Discord.send(text, { id: channel.channelId }, { embeds });
 		}
 	}
 });
