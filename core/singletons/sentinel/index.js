d9ff343410ec4d7765c410006ef241c057d2c04b
torikushiii <21153445+torikushiii@users.noreply.github.com>
2023-12-17 02:23:58 +0800

    refactor Sentinel class properties and moved crons to it own directory

--- core/singletons/sentinel/index.js
+++ core/singletons/sentinel/index.js
@@ -1,15 +1,14 @@
 const User = require("./user.js");
 const Events = require("./events.js");
-const TimelineFetcher = require("./timeline-fetcher.js");
 const Template = require("../template.js");
 
 const { CronJob } = require("cron");
 const channelList = require("../../../channels.json");
 
 module.exports = class SentinelSingleton extends Template {
-	#firstRun = true;
+	firstRun = true;
 	#ignoreList = [];
-	#restartRequested = false;
+	restartRequested = false;
 
 	ignoreListExpiration = 3_600_000;
 	// eslint-disable-next-line no-return-assign
@@ -29,55 +28,8 @@ module.exports = class SentinelSingleton extends Template {
 	constructor () {
 		super();
 
-		this.crons = [
-			{
-				time: "*/5 * * * * *",
-				fn: this.fetchTimeline.bind(this)
-			},
-			{
-				time: "* * * * *",
-				fn: this.removeInactiveChannels.bind(this)
-			},
-			{
-				time: "*/5 * * * *",
-				fn: this.checkForMissingChannels.bind(this)
-			},
-			{
-				time: "* * * * *",
-				fn: this.restart.bind(this)
-			},
-			{
-				time: "*/30 * * * * *",
-				fn: this.suggestionNotification.bind(this)
-			}
-		];
-
-		this.init();
-	}
-
-	async init () {
-		for (const cron of this.crons) {
-			const job = new CronJob(cron.time, cron.fn);
-			job.start();
-		}
-	}
-
-	async fetchTimeline () {
-		const userList = await this.getUsers();
-		if (userList.length === 0) {
-			throw new app.Error({ message: "No users found" });
-		}
-
-		const tf = new TimelineFetcher(userList);
-		const timeline = await tf.fetch({ firstRun: this.#firstRun });
-
-		const isRateLimited = timeline.every(i => i.length === 0);
-		if (isRateLimited) {
-			app.Logger.warn("SentinelModule", "All tweets returned empty, rate limited (?)");
-			return;
-		}
-
-		await this.processTweets(timeline);
+		this.cron = new CronJob("* * * * *", () => this.restart());
+		this.cron.start();
 	}
 
 	async processTweets (tweetData) {
@@ -114,15 +66,15 @@ module.exports = class SentinelSingleton extends Template {
 				{ expireAt: 0 }
 			);
 
-			if (!this.#firstRun) {
+			if (!this.firstRun) {
 				for (const tweet of newTweets) {
 					Events.emit("new-tweet", tweet);
 				}
 			}
 		}
 
-		if (this.#firstRun) {
-			this.#firstRun = false;
+		if (this.firstRun) {
+			this.firstRun = false;
 			app.Logger.info("SentinelModule", `Sentinel is now watching ${tweetData.length} users`);
 		}
 	}
@@ -152,7 +104,7 @@ module.exports = class SentinelSingleton extends Template {
 		}
 
 		app.Logger.info("SentinelModule", `Adding ${newChannels.length} new channels.`);
-		this.#restartRequested = true;
+		this.restartRequested = true;
 
 		await app.Cache.setByPrefix(
 			"twitter-channels",
@@ -253,9 +205,9 @@ module.exports = class SentinelSingleton extends Template {
 
 		return users;
 	}
-
+	
 	async restart () {
-		if (!this.#restartRequested) {
+		if (!this.restartRequested) {
 			return;
 		}
 
@@ -264,135 +216,6 @@ module.exports = class SentinelSingleton extends Template {
 
 		const pm2 = app.Config.get("PM2_NAME");
 		await shell(`pm2 restart ${pm2}`);
-	}
-
-	async suggestionNotification () {
-		const subscriptions = await app.Query.collection("suggestions")
-			.find({ status: "completed", fired: false })
-			.toArray();
-
-		if (subscriptions.length === 0) {
-			return;
-		}
-
-		for (const subscription of subscriptions) {
-			const { user, authorNote } = subscription;
-			const userId = user.id;
-
-			const baseText = "Your suggestion has been completed";
-			const note = (authorNote) ? `${baseText}.\n\n${authorNote}` : `${baseText}.`;
-
-			const embeds = [
-				{
-					title: "Suggestion Completed",
-					description: note,
-					color: 0x00FF00
-				}
-			];
-
-			await app.Query.collection("suggestions").updateOne({ _id: subscription._id }, { $set: { fired: true } });
-
-			app.Logger.info("SentinelModule", `Sending suggestion notification to ${userId}`);
-			const userData = await app.Discord.client.users.fetch(userId);
-			await userData.send({ embeds });
-		}
-	}
-
-	async checkForMissingChannels () {
-		const cachedChannels = await this.getUsers({ usernameOnly: true });
-		const guilds = await app.Cache.getKeysByPrefix("discord-guilds-*");
-		if (guilds.length === 0) {
-			return;
-		}
-
-		const allChannels = [];
-		for (const guild of guilds) {
-			const guildData = await app.Cache.getByPrefix(guild);
-			const { channels } = guildData;
-			if (channels.length === 0) {
-				continue;
-			}
-
-			allChannels.push(channels.map(i => i.username));
-		}
-
-		const channels = allChannels.flat();
-		const missingChannels = channels.filter(i => !cachedChannels.includes(i));
-
-		if (missingChannels.length === 0) {
-			return;
-		}
-
-		const data = [];
-		for (const channel of missingChannels) {
-			if (this.#ignoreList.includes(channel.toLowerCase())) {
-				continue;
-			}
-
-			let userData = await app.User.get(channel);
-			if (!userData) {
-				userData = await this.fetchUser(channel);
-				if (userData.success === false && userData?.error?.code === "NO_USER_FOUND") {
-					this.#ignoreList.push(channel.toLowerCase());
-					continue;
-				}
-			}
-
-			if (userData?.success === false) {
-				this.#ignoreList.push(channel.toLowerCase());
-				continue;
-			}
-
-			data.push(userData.username.toLowerCase());
-		}
-
-		if (data.length === 0) {
-			return;
-		}
-
-		await app.Cache.setByPrefix(
-			"twitter-channels",
-			[...cachedChannels, ...data],
-			{ expireAt: 0 }
-		);
-
-		app.Logger.info("SentinelModule", `Added ${data.length} missing channels.`);
-	}
-
-	async removeInactiveChannels () {
-		const cachedChannels = await this.getUsers({ usernameOnly: true });
-		const guilds = await app.Cache.getKeysByPrefix("discord-guilds-*");
-		if (guilds.length === 0) {
-			return;
-		}
-
-		const allChannels = [];
-		for (const guild of guilds) {
-			const guildData = await app.Cache.getByPrefix(guild);
-			const { channels } = guildData;
-			if (channels.length === 0) {
-				continue;
-			}
-
-			allChannels.push(channels.map(i => i.username));
-		}
-
-		const channels = allChannels.flat();
-		const inactiveChannels = cachedChannels.filter(i => !channels.includes(i));
-
-		if (inactiveChannels.length === 0) {
-			return;
-		}
-
-		const cleanedChannels = cachedChannels.filter(i => !inactiveChannels.includes(i));
-		await app.Cache.setByPrefix(
-			"twitter-channels",
-			cleanedChannels,
-			{ expireAt: 0 }
-		);
-
-		await this.purgeUsers(inactiveChannels);
-		app.Logger.info("SentinelModule", `Removed ${inactiveChannels.length} inactive channels.`);
 	}
 
 	get modulePath () { return "sentinel"; }
